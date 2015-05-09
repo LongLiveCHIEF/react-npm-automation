a7c14e131515989b4b3036cefd7f940238d0868d
Brian Vanderbusch <brian@normalgravity.com>
2015-05-09 14:59:18 -0500

    adding more specs to the hello component

--- test/hello-test.js
+++ test/hello-test.js
@@ -1,15 +1,42 @@
+/** @jsx React.DOM */
 var jsdom = require('mocha-jsdom');
 var expect = require('chai').expect;
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
 var Hello = require('../src/hello.jsx');
 
-describe('mocha tests', function (){
+describe('Setup test', function (){
 
 	jsdom({parsingMode: 'xml'});
 
-	it('has document', function() {
+	// render </Hello > into jsdom
+	
+	before('rendering and locating element', function (){
+		// render <Hello /> into jsdom
+		var helloComponent = TestUtils.renderIntoDocument(
+			<Hello packageName="test" />
+		);
+				
+		var helloDiv = TestUtils.findRenderedDOMComponentWithTag(helloComponent, 'div');
+
+		this.helloElement = helloDiv.getDOMNode();
+	});
+
+	
+	it('jsdom is working', function() {
 		var div = document.createElement('div');
 		expect(div.nodeName).eql('div');
 	
 	});
+
+	it('should render a success message',function(){
+		expect(this.helloElement).to.contain('has been set up');
+	});
+
+	it('should show package name in message',function(){
+		expect(this.helloElement).to.contain('test');	
+	});
+
+
 
 });
--- test/jsx-compiler.js
+++ test/jsx-compiler.js
@@ -3,7 +3,7 @@ var ReactTools = require('react-tools');
 
 require.extensions['.jsx'] = function(module, filename){
 	var content = fs.readFileSync(filename, 'utf8');
-	var compiled = ReactTools.transform(content /*, {harmony: true} */);
+	var compiled = ReactTools.transform(content);
 
 	return module._compile(compiled, filename);
 };
