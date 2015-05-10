2ecc391ea8b7b3199420ff9148edacf80b44fc19
Brian Vanderbusch <brian@normalgravity.com>
2015-05-09 21:30:16 -0500

    adding watching, closes #2

--- src/hello.jsx
+++ src/hello.jsx
@@ -1,22 +1,22 @@
-var React = require('react/addons');
+var React = require("react/addons");
 
 
-// simple component to ensure setup is working
+// a simple component to ensure setup is working
 
 var Hello = React.createClass({
 	getDefaultProps: function(){
 		return {
-			packageName:'React &amp; Flux NPM Automation'
-		}
+			packageName: "React &amp; Flux NPM Automation"
+		};
 	},
 	getInitialState: function(){
 		return {
-			packageName: 'React &amp; Flux NPM Automation'
-		}
+			packageName: "React &amp; Flux NPM Automation"
+		};
 	},
 	render: function() {
 		return (
-			<div>{this.props.packageName}, has been set up and is running!</div>
+			<div>{this.props.packageName},  has been set up and is running!</div>
 		);
 	}
 });
