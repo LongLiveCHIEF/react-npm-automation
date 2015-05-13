0c9d6263557530c051bfa999eca52ee77140f961
Brian Vanderbusch <brian@normalgravity.com>
2015-05-12 22:24:26 -0500

    setup test rebuilt using domino and Zepto

--- test/hello-spec.jsx
+++ test/hello-spec.jsx
@@ -4,16 +4,27 @@ var Hello = require('../src/client/hello.jsx');
 describe('Setup test', function (){
 
 	var node, message;
+	var packageName = 'test';
 
 	// render </Hello > into jsdom
 	before('rendering component', function(){
 		var component = React.render(
-			<Hello packageName="test"/>,
+			<Hello packageName={packageName} />,
 			document.body);
+	
+		var component = React.test.findRenderedDOMComponentWithTag(component, 'div');
+		message = component.getDOMNode().textContent;
 	});	
 	
 	it('domino is working', function() {
 		expect(true).to.be.ok;
 	});
 
+	it('should render a success message', function(){
+		expect(message).to.contain('has been set up');
+	});
+
+	it('should include the package name', function(){
+		expect(message).to.contain(packageName);
+	});
 });
