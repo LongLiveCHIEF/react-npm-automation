db220ad14b7d45ec3f05039f6f4be9a447118016
Brian Vanderbusch <brian@normalgravity.com>
2015-05-13 20:12:19 -0500

    howdy there

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var PORT = process.env.DEV_PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'build')));
+var server = app.listen(PORT, function(){
+	process.stdout.write("listening on " + PORT + ", serving: " + path.join(__dirname, '../build/\n'));
+});
+
+var home = express.Router();
+
+home.get('/',function(req, res, next){
+	process.stdout.write("request logged \n");	
+	res.send('Howdy there!');
+});
+
+app.use('/',home);
 
 app.listen(3000);
-console.log("listening on port 3000");
