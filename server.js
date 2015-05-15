1af9cb4050a7b3494c17edfb6bebc98f7b16da8f
Brian Vanderbusch <brian@normalgravity.com>
2015-05-15 16:20:56 -0500

    update deps and start scripts, repositioned livereload
    Livereload, based on the way express works with it's  methods, needs to be directly before the static middleware

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-var express = require("express"),
-    app = express(),
+var express = require("express");
+var app = express(),
     bodyParser = require('body-parser'),
     errorHandler = require('errorhandler'),
     methodOverride = require('method-override'),
     hostname = process.env.HOSTNAME || 'localhost',
     port = parseInt(process.env.PORT, 10) || 7654,
     publicDir = process.argv[2] || __dirname + '/build';
-
-app.use(require("connect-livereload")());
 
 app.get("/", function (req, res) {
   res.redirect("/index.html");
@@ -18,11 +16,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(require("connect-livereload")());
 app.use(express.static(publicDir));
 app.use(errorHandler({
   dumpExceptions: true,
   showStack: true
 }));
-console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
+console.log("You are now serving static files from %s listening at http://%s:%s", publicDir, hostname, port);
 app.listen(port, hostname);
 
