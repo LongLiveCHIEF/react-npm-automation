de8b05dd580bbd606457da1d4bb5ee9cf40dc17c
Brian Vanderbusch <brian@normalgravity.com>
2015-05-18 21:55:34 -0500

    first test basic element connected to store

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,11 +1,29 @@
 var React = require("react/addons");
 var Hello = require("./components/hello.jsx");
 var pkg = require("../../package.json");
+var Reflux = require("reflux");
 
 window.LiveReloadOptions = { host: "localhost" };
 require('livereload-js');
 
+var packageStore = Reflux.createStore({
+	data: {pkg},
+	getInitialState: function(){
+		return this.data;
+	}
+});
+
+var App = React.createClass({
+
+	mixins: [Reflux.connect(packageStore)],
+	render: function(){
+		return (
+			<div>Package Name: {this.state.pkg.name} </div>
+		);
+	}
+});
+
 React.render(
-<Hello packageName={pkg.name} />,
+<App />,
 	document.body
 );
